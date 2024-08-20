8984f3e93491fb8eef106be9e945242ffc3758fb
Hasitha Priyasad Wijerathna <hasithapriyasadmail@gmail.com>
2024-08-21 00:53:15 +0530

    task Update Controller added

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -3,6 +3,7 @@ import { taskService } from '../services/task.service';
 import { CreateTaskDTO } from '../dtos/create-task.dto';
 import { handle } from './helper';
 import httpStatus from 'http-status';
+import { UpdateTaskDTO } from '../dtos/update-task.dto';
 class TaskController {
     createTask(req: Request<{},{},CreateTaskDTO>, res: Response, next: NextFunction): void {
         handle(req, res, next, taskService.createTask, httpStatus.CREATED);
@@ -14,6 +15,10 @@ class TaskController {
 
     deleteTask(req: Request< { id: string }, {}, {}>, res: Response, next: NextFunction): void { 
         handle(req, res, next, taskService.deleteTask, httpStatus.NO_CONTENT);
+    }
+
+    updateTask(req: Request< { id: string }, {}, UpdateTaskDTO>, res: Response, next: NextFunction): void {
+        handle(req, res, next, taskService.updateTask, httpStatus.OK);
     }
 }
 
