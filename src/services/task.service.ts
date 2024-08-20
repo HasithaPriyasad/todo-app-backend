b706be8358d3dfced6e3f65087a6ad1848f2958e
Hasitha Priyasad Wijerathna <hasithapriyasadmail@gmail.com>
2024-08-21 00:53:03 +0530

    taskUpdate function added to the service

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -2,10 +2,9 @@ import { randomUUID } from "crypto";
 import { ITask, Task } from "../models/task.model";
 import { taskRepository } from "../repositories/task.repository";
 import { CreateTaskDTO } from "../dtos/create-task.dto";
-import NotFoundError from "../errors/not-found.error";
 import { ErrorStatus } from "../enums/error-status.enum";
-import { Request } from "express";
-
+import { UpdateTaskDTO } from "../dtos/update-task.dto";
+import NotFoundError from "../errors/not-found.error";
 class TaskService {
     createTask({title, status }: CreateTaskDTO):ITask {
         const task = new Task(randomUUID(), title, status);
@@ -23,6 +22,15 @@ class TaskService {
             throw new NotFoundError(ErrorStatus.TASK_NOT_FOUND, 'id', {id: id});
         }
         return taskRepository.deleteTask(id);
+    }
+
+    updateTask(body: UpdateTaskDTO, params: { id: string }): ITask | undefined {
+        const {id} = params;
+        const task = taskRepository.findById(id);
+        if (!task) {
+            throw new NotFoundError(ErrorStatus.TASK_NOT_FOUND, 'id', {id: id});
+        }
+        return taskRepository.updateTask(id, body);
     }
 }
 
